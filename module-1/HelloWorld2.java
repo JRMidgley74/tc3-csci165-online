9351b676d79fb675e803e01bd8000f739242de20
Jeremy Midgley <JRM020@mymail.tc3.edu>
2020-02-01 23:39:00 -0500

    Submitting for Module 1

---
+++ module-1/HelloWorld2.java
@@ -0,0 +1,10 @@
+public class HelloWorld2{
+
+	public static void main(String[] args){
+
+		System.out.print("Hello ");
+		System.out.print(args[0] + " ");
+		System.out.print(args[1] + "");
+		System.out.println(". Nice work processing the arguments");
+	}
+}
